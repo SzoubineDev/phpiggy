72ed31128b70a990ce8e245d6851942c5f9755de
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-05 18:07:33 +0100

    adding the Router class

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -2,6 +2,11 @@
 declare(strict_types = 1);
 namespace Framework;
 class App{
+    private Router $router;
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
     function run(){
         echo "application is running";
 }
