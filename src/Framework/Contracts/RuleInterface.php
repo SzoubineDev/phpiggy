ef6fe87548fe11097d829b8b98214297a72dff55
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-22 17:01:18 +0100

    RuleInterface

--- src/Framework/Contracts/RuleInterface.php
+++ src/Framework/Contracts/RuleInterface.php
@@ -7,4 +7,5 @@ namespace Framework\Contracts;
 interface RuleInterface
 {
     public function validate(array $data, string $feild, array $params): bool;
+    public function getMessage(array $data, string $feild, array $params): string;
 }
