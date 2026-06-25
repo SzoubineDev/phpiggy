c1772655d4ce8d85f42802a874ea29889bfc0dfb
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-25 15:34:31 +0100

    storing errors in an array

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -13,5 +13,21 @@ class Validator
     {
         $this->rules[$alias] = $rule;
     }
-    public function validate(array $formData, array $feilds) {}
+    public function validate(array $formData, array $feilds)
+    {
+        $errors = [];
+        foreach ($feilds as $feildName => $rules) {
+
+            foreach ($rules as $rule) {
+                $formValidator = $this->rules[$rule];
+                if ($formValidator->validate($formData, $feildName, [])) {
+                    continue;
+                }
+                $errors[$feildName][] = $formValidator->getMessage($formData, $feildName, []);
+            }
+        }
+        if (count($errors)) {
+            dd($errors);
+        }
+    }
 }
