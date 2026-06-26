ff9b371f2695c728cb476a3cffe183f394671a76
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-26 08:42:32 +0100

    paginator gone

--- public/index.php
+++ public/index.php
@@ -1,6 +1,35 @@
 <?php
 
-declare(strict_types=1);
-include __DIR__ . "/../src/App/functions.php";
-$app = include __DIR__ . "/../src/App/bootstrap.php";
-$app->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// declare(strict_types=1);
+// include __DIR__ . "/../src/App/functions.php";
+// $app = include __DIR__ . "/../src/App/bootstrap.php";
+// $app->run();
