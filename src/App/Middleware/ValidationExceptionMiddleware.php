aaf2c64d75bdf5819fdce5ba09ef5ace1145c247
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-29 11:43:17 +0100

    first implementation process()

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -6,3 +6,12 @@ namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
 use Framework\Exceptions\ValidationException;
+use Override;
+
+class ValidationExceptionMiddleware implements MiddlewareInterface
+{
+    public function process(callable $next)
+    {
+        $next();
+    }
+}
