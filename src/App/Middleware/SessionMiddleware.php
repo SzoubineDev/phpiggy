a4787cffb58c99de03c2fda3d63a7624460d197e
szoubinedev <zoubinesoulaimane@gmail.com>
2026-06-30 09:43:53 +0100

    sessionException

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Exceptions\SessionException;
 use Framework\Contracts\MiddlewareInterface;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("session is currently active");
+        }
         session_start();
         $next();
     }
