1f6611c3a7f162aa373e20334dfb9162eba675c0
szoubinedev <zoubinesoulaimane@gmail.com>
2026-08-16 13:51:14 +0100

    establishing a database connextion

--- cli.php
+++ cli.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "hello world";
-
-
+$driver = 'mydijdsql';
+$config = http_build_query(data: [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => "phpiggy"
+], arg_separator: ';');
+$dsn =  "{$driver}:{$config}";
+$username = 'root';
+$password = '';
+$db = new PDO($dsn, $username, $password);
+echo "connected successfully !";
