b706776379231ee783e2cf3a834e3748abdce378
szoubinedev <zoubinesoulaimane@gmail.com>
2026-09-11 15:25:27 +0100

    changing troutes order

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -12,7 +12,7 @@ function registerRoutes(App $app)
     $app->get('/', [HomeController::class, 'home']);
     $app->get('/about', [AboutController::class, 'about']);
     $app->get('/register', [AuthController::class, 'registerView']);
+    $app->post('/register', [AuthController::class, 'register']);
     $app->get('/login', [AuthController::class, 'loginView']);
     $app->post('/login', [AuthController::class, 'login']);
-    $app->post('/register', [AuthController::class, 'register']);
 }
