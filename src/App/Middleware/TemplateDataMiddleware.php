b3b3e1b208624ff9e354a9eb882b75da1096f090
szoubinedev <zoubinesoulaimane@gmail.com>
2026-09-13 01:40:29 +0100

    template amend

--- src/App/Middleware/TemplateDataMiddleware.php
+++ src/App/Middleware/TemplateDataMiddleware.php
@@ -12,7 +12,6 @@ class TemplateDataMiddleware implements MiddlewareInterface
     public function __construct(private TemplateEngine $view) {}
     public function process(callable $next)
     {
-        $this->view->addGlobal('title', 'Exepense tarcking app');
         $next();
     }
 }
