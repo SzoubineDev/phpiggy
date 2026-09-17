e60626e41474e5ea917c7dadefbb8d8158b40d93
szoubinedev <zoubinesoulaimane@gmail.com>
2026-09-17 23:40:40 +0100

    implementing invalid date

--- src/Framework/Rules/DateFormatRule.php
+++ src/Framework/Rules/DateFormatRule.php
@@ -9,7 +9,14 @@ use Framework\Contracts\RuleInterface;
 class DateFormatRule implements RuleInterface
 {
 
-    public function validate(array $data, string $feild, array $params): bool {}
-    public function getMessage(array $data, string $feild, array $params): string {}
-}
+    public function validate(array $data, string $feild, array $params): bool
+    {
 
+        $parsedDate = date_parse_from_format($params[0], $data[$feild]);
+        return $parsedDate['error_count'] === 0 || $parsedDate['warning_count'] === 0;
+    }
+    public function getMessage(array $data, string $feild, array $params): string
+    {
+        return "Invalid date";
+    }
+}
