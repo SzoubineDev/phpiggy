8e1dd0b2405846055d1ca7c7dd512237fbd18b17
szoubinedev <zoubinesoulaimane@gmail.com>
2026-09-18 00:06:16 +0100

    create query

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -9,4 +9,17 @@ use Framework\Database;
 class TransactionService
 {
     public function __construct(private Database $db) {}
+    public function create(array $formData)
+    {
+        $this->db->query(
+            "INSERT INTO transactions (user_id,description,amount,date)
+             VALUES (:user_id,:description,:amount,:date)",
+            [
+                'user_id' => $_SESSION['user'],
+                'description' => $formData['description'],
+                'amount' => $formData['amount'],
+                'date' => $formData['date']
+            ]
+        );
+    }
 }
